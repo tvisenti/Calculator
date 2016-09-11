88855653b612a0755560af7a6f8bfbecfffcd521
Thomas Visentin <tho.visentin@gmail.com>
2016-09-11 17:30:53 +0200

    First Calculator

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Thomas Visentin on 11/09/16.
+//  Copyright © 2016 Thomas Visentin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    @IBAction private func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        displayValue = brain.result
+    }
+}
---
+++ CalculatorBrain.swift
@@ -0,0 +1,73 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Thomas Visentin on 11/09/16.
+//  Copyright © 2016 Thomas Visentin. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    private var accumulator = 0.0
+    
+    func setOperand(operand: Double) {
+        accumulator = operand
+    }
+    
+    private var operations: Dictionary<String,Operation> = [
+        "π" : Operation.Constant(M_PI),
+        "℮" : Operation.Constant(M_E),
+        "√" : Operation.UnaryOperation(sqrt),
+        "cos" : Operation.UnaryOperation(cos),
+        "×" : Operation.BinaryOperation({$0 * $1}),
+        "+" : Operation.BinaryOperation({$0 + $1}),
+        "−" : Operation.BinaryOperation({$0 - $1}),
+        "÷" : Operation.BinaryOperation({$0 / $1}),
+        "=" : Operation.Equals
+    ]
+    
+    private enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+    }
+    
+    func performOperation(symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Constant(let value):
+                accumulator = value
+            case .UnaryOperation(let foo):
+                accumulator = foo(accumulator)
+            case .BinaryOperation(let foo):
+                executePendingBinaryOperation()
+                pending = PendingBinaryOperationInfo(binaryFunction: foo, firstOperand: accumulator)
+            case .Equals:
+                executePendingBinaryOperation()
+            }
+        }
+    }
+    
+    private func executePendingBinaryOperation() {
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+
+    private var pending: PendingBinaryOperationInfo?
+    
+    private struct PendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOperand: Double
+    }
+    
+    var result: Double {
+        get {
+            return accumulator
+        }
+    }
+}
